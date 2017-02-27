7a6a711e6f3b1547368bfbfb4318699bb0d4ab37
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2017-02-27 08:46:47 +0100

    Adding dev.js script for testing MS validation more effectively. Also adding example JWKS and MS.

--- dev.js
+++ dev.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const Federation = require('./lib/Federation');
 
 const f = new Federation();
@@ -10,3 +11,16 @@ f.setupKeys()
   .catch((err) => {
     console.error(err);
   });
+
+
+const ms = fs.readFileSync('./var/example-ms.txt', 'utf8');
+const jwks = JSON.parse(fs.readFileSync('./var/example-jwks.json', 'utf8'));
+
+f.decode(jwks, ms)
+.then((result) => {
+  console.log('----- Result ----');
+  console.log(result);
+})
+.catch((err) => {
+  console.error('Error', err);
+});
